8438d23795b4f45f206c172e7671402ddadf7844
Sansr1val <110670707+Sansr1val@users.noreply.github.com>
2022-10-27 10:57:03 +0800

    Added string in the display

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,84 +1,70 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 
-public class Main {
-		static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		static LinkedList list = new LinkedList();
-	public static void main(String[] args) {
-		int quantity;
-		while(true) {
-			try {
-				System.out.println("The quantity of numbers to be inserted into the list: ");
-				quantity = Integer.parseInt(in.readLine());
-				if(quantity<0) {
-					System.out.println("Only integers >= 0 is allowed.");
-				}else {
-					break;
-				}
-			} catch (Exception e) {
-				System.out.println("Invalid input! Try again.");
-			}
-		}
-		
-		for(int i =1; i<=quantity; i++) {
-			while(true) {
-				try {
-					System.out.print("Enter a number: ");
-					int number = Integer.parseInt(in.readLine());
-					list.Append(number);
-					break;
-				} catch (Exception e) {
-					System.out.println("Invalid input! Try again.");
-				}
-			}
-		}
-		System.out.println("The linked list is successfully created!");
-		
-		while(true) {
-			System.out.println("\nWhat do you want to do with the list?");
-			System.out.println("1) Display");
-			System.out.println("2) Append");
-			System.out.println("3) Insert After");
-			System.out.println("4) Insert Before");
-			System.out.println("5) Delete");
-			System.out.println("6) Exit");
-			int command;
-			while(true) {
-				try {
-					System.out.print("\nEnter your Command:");
-					command = Integer.parseInt(in.readLine());
-					break;
-				} catch (Exception e) {
-					System.out.println("Invalid input!");
-				}
-			}
-			
-			switch (command){
-			case 2:
-				list.Display();
-			break;
-			
-			case 3:
-				DeleteAll();
-			break;
-				
-			case 4:
-				System.out.println("Thank you!");
-				System.exit(0);
-			break;
-			}
+public class LinkedList {
+	private Node head;
+	private Node tail;
+	
+	public Node getTail() {
+		return tail;
+	}
+
+	public void setTail(Node tail) {
+		this.tail = tail;
+	}
+
+	public Node getHead() {
+		return head;
+	}
+
+	public void setHead(Node head) {
+		this.head = head;
+	}
+	
+	public void Append(int data) {
+		Node node = new Node(data);
+		if(head == null) {
+			head = node;
+			tail = node;
+		}else {
+			tail.setNext(node);
+			node.setPrevious(tail);
+			tail = node;
 		}
 	}
 	
+	public void Display() {
+		Node currentNode = head;
+		String str = "";
+		while(currentNode != null) {
+			str += String.valueOf(currentNode.getData()) +", ";
+			currentNode = currentNode.getNext();
+		}
+		if(str !="") {
+			str = str.substring(0,str.length() -2);
+		}
+		System.out.println("The values in the linked list are: "+str);
+	}
 	
-	public static void DeleteAll() {
-		try {
-			System.out.println("Enter the number to be deleted: ");
-			int number = Integer.parseInt(in.readLine());
-			list.DeleteAll(number);
-		} catch (Exception e) {
-			System.out.println("Invalid input! Try again.");
+	public void DeleteAll(int data) {
+		
+		while(head != null && head.getData() == data) {
+			head = head.getNext();
+		}
+		while(tail != null && tail.getData() == data) {
+			tail = tail.getPrevious();
+		}
+		Node currentNode = head;
+		
+		while(currentNode.getNext() != null && currentNode != null) {
+			if(currentNode.getNext().getData() == data) {
+				Node nodeToDelete = currentNode.getNext();
+				Node nextNode = currentNode.getNext().getNext();
+				currentNode.setNext(nextNode);
+				nextNode.setPrevious(currentNode);
+				nodeToDelete.setNext(null);
+				nodeToDelete.setPrevious(null);
+			}else {
+				currentNode = currentNode.getNext();
+			}
 		}
 	}
-
 }
