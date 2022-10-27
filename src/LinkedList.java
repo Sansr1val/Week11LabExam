062b5d4eb6b7247cd49f378d9108c4597564e571
Sansr1val <110670707+Sansr1val@users.noreply.github.com>
2022-10-27 11:48:19 +0800

    Update LinkedList.java

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,45 +1,80 @@
 
-public class Node {
+public class LinkedList {
+	private Node head;
+	private Node tail;
 	
-	private int data;
-	private Node next;
-	Node previous;
-	
-	//Constructors
-	public Node() {
-		this.data = 0;
-		this.next = null;
-		this.previous = null;
-	}
-	
-	public Node(int data) {
-		this.data = data;
-		this.next = null;
-		this.previous = null;
+	//setters and getters
+	public Node getTail() {
+		return tail;
 	}
 
-	//setters and getters.
-	public int getData() {
-		return data;
+	private void setTail(Node tail) {
+		this.tail = tail;
+	}
+
+	public Node getHead() {
+		return head;
+	}
+
+	private void setHead(Node head) {
+		this.head = head;
 	}
 	
-	public void setData(int data) {
-		this.data = data;
+	//Adds the new node at the end of the list.
+	public void Append(int data) {
+		Node node = new Node(data);
+		if(head == null) {
+			head = node;
+			tail = node;
+		}else {
+			tail.setNext(node);
+			node.setPrevious(tail);
+			tail = node;
+		}
 	}
 	
-	public Node getNext() {
-		return next;
-	}
 	
-	public void setNext(Node next) {
-		this.next = next;
+	/*
+	 * Deletes all the integers that is a duplicate of the data input
+	 * Traverses through the list to find the duplicates and unlinks them from the link
+	 * If the head is equal to the data input, we move the head pointer to the next node
+	 * to unlink the previous head node.
+	 * The same goes for the tail node.
+	 */
+	public void DeleteAll(int data) {
+		while(head != null && head.getData() == data) {
+			head = head.getNext();
+		}
+		while(tail != null && tail.getData() == data) {
+			tail = tail.getPrevious();
+		}
+		Node currentNode = head;
+		
+		while(currentNode.getNext() != null && currentNode != null) {
+			if(currentNode.getNext().getData() == data) {
+				Node nodeToDelete = currentNode.getNext();
+				Node nextNode = currentNode.getNext().getNext();
+				currentNode.setNext(nextNode);
+				nextNode.setPrevious(currentNode);
+				nodeToDelete.setNext(null);
+				nodeToDelete.setPrevious(null);
+			}else {
+				currentNode = currentNode.getNext();
+			}
+		}
+		
 	}
-	
-	public Node getPrevious() {
-		return previous;
-	}
-	
-	public void setPrevious(Node previous) {
-		this.previous = previous;
+	//Displays the contents of the list
+	public void Display() {
+		Node currentNode = head;
+		String str = "";
+		while(currentNode != null) {
+			str += String.valueOf(currentNode.getData()) +", ";
+			currentNode = currentNode.getNext();
+		}
+		if(str !="") {
+			str = str.substring(0,str.length() -2);
+		}
+		System.out.println("The values in the linked list are: "+str);
 	}
 }
